832c13524152620320b90727467b7c591151c3ad
Jacob Conrad <Jacob.Conrad@quinnipiac.edu>
2021-02-08 17:08:37 -0500

    Added two more GitHelloWorld objects

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	{
 		System.out.println("Hello World!");
 		GitHelloWorld one = new GitHelloWorld(1);
+		GitHelloWorld two = new GitHelloWorld(2);
+		GitHelloWorld three = new GitHelloWorld(3);
 		System.out.println("How are you?");
 	}
 }
