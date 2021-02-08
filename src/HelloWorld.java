85c32773f83b4f1aefd703782b9c7f5329e1c2df
Jacob Conrad <Jacob.Conrad@quinnipiac.edu>
2021-02-08 16:59:13 -0500

    Created a GitHelloWorld object named one with value 1 in the HelloWorld class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World!");
+		GitHelloWorld one = new GitHelloWorld(1);
 		System.out.println("How are you?");
 	}
 }
